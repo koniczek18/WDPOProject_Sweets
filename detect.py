a61eb0c4cfe9747866cc89629e1c4e4fc5ca962b
koniczek <sz.zalas@gmail.com>
2023-01-25 17:02:34 +0100

    Initial commit

--- detect.py
+++ detect.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 from typing import Dict
+import numpy as np
 
 import click
 import cv2
@@ -9,18 +10,32 @@ from tqdm import tqdm
 
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
-
+    ###
     Parameters
     ----------
     img_path : str
         Path to processed image.
-
+    ###
     Returns
     -------
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    img = cv2.imread(img_path, cv2.IMREAD_COLOR)
+
+    lower_yellow = np.array([15, 50, 180])
+    upper_yellow = np.array([40, 255, 255])
+    def empty_callback(value):
+        pass
+    OriginalImage = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    hsv = cv2.cvtColor(OriginalImage, cv2.COLOR_BGR2HSV)
+    cv2.namedWindow('image')
+    mask = cv2.inRange(hsv, lower_yellow, upper_yellow)
+    result = cv2.bitwise_and(OriginalImage, OriginalImage, mask=mask)
+    while True:
+        cv2.imshow('image', mask)
+        key_code = cv2.waitKey(10)
+        if key_code == 27:
+            break
 
     #TODO: Implement detection method.
     
