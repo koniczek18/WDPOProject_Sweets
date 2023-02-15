813cc7fb1ee0ec92f1edc3139462ac8558b09093
koniczek <sz.zalas@gmail.com>
2023-02-15 20:39:23 +0100

    Finalisation

--- detect.py
+++ detect.py
@@ -6,97 +6,104 @@ import click
 import cv2
 from tqdm import tqdm
 
+#DEBUG BEGIN
+debug_enabled=False
+'''
+cv2.namedWindow('image')
+cv2.namedWindow('mask')
+def empty_callback(value):
+    pass
+cv2.createTrackbar('H-Lower', 'image', 40, 179,empty_callback)
+cv2.createTrackbar('S-Lower', 'image', 0, 255,empty_callback)
+cv2.createTrackbar('V-Lower', 'image', 9, 255,empty_callback)
+cv2.createTrackbar('H-Higher', 'image', 80, 179,empty_callback)
+cv2.createTrackbar('S-Higher', 'image', 255, 255,empty_callback)
+cv2.createTrackbar('V-Higher', 'image', 255, 255,empty_callback)
+'''
+#DEBUG END
 
-#IMAGE PREPROCESSING
-def imageInitialProcessing(image,brightnessBump=30):
+def brightnessBump(bgr,bump):
+    hsv = cv2.cvtColor(bgr, cv2.COLOR_BGR2HSV)
+    h, s, v = cv2.split(hsv)
+    limiter = 255 - bump
+    v[v > limiter] = 255
+    v[v <= limiter] += bump
+    hsv = cv2.merge((h, s, v))
+    img = cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)
+    return bgr,hsv
+
+def adjust_gamma(image, gamma=1.0):
+	invGamma = 1.0 / gamma
+	table = np.array([((i / 255.0) ** invGamma) * 255 for i in np.arange(0, 256)]).astype("uint8")
+	return cv2.LUT(image, table)
+
+def imageInitialProcessing(image,colorType='null'):
     #RESIZE
     dim=(720,int((image.shape[0])*720/image.shape[1]))
     resized = cv2.resize(image, dim, interpolation=cv2.INTER_AREA)
-    #BLUR
-    blured=cv2.medianBlur(resized,5)
-    #HSV
-    hsv = cv2.cvtColor(blured, cv2.COLOR_BGR2HSV)
-    #BRIGHT
-    limit=255-brightnessBump
-    h,s,v=cv2.split(hsv)
-    v[v>limit]=255
-    v[v<=limit]+=brightnessBump
-    brightenedHSV=cv2.merge((h, s, v))
-    brightenedBGR = cv2.cvtColor(brightenedHSV, cv2.COLOR_HSV2BGR)
-    return resized,blured,hsv,brightenedHSV,brightenedBGR
+    if colorType=='Yellow':
+        bgr = cv2.normalize(resized, None, alpha=50, beta=200, norm_type=cv2.NORM_MINMAX)
+        bgr = cv2.dilate(bgr, np.ones((3, 3), np.uint8))
+        bgr = cv2.medianBlur(bgr, 5)
+        bgr, hsv=brightnessBump(bgr,250)
+        bgr=adjust_gamma(bgr,0.1)
+        bgr, hsv = brightnessBump(bgr, 0)
+        bgr = adjust_gamma(bgr, 4)
+    elif colorType=='Green':
+        bgr = cv2.normalize(resized, None, alpha=50, beta=200, norm_type=cv2.NORM_MINMAX)
+        bgr = cv2.dilate(bgr, np.ones((3, 3), np.uint8))
+        bgr = cv2.medianBlur(bgr, 5)
+        bgr, hsv=brightnessBump(bgr,250)
+        bgr=adjust_gamma(bgr,0.1)
+        bgr, hsv = brightnessBump(bgr, 0)
+        bgr = adjust_gamma(bgr, 4)
+    elif colorType=='Red':
+        bgr = cv2.normalize(resized, None, alpha=50, beta=200, norm_type=cv2.NORM_MINMAX)
+        bgr = cv2.dilate(bgr, np.ones((3, 3), np.uint8))
+        bgr = cv2.medianBlur(bgr, 5)
+        bgr, hsv=brightnessBump(bgr,250)
+        bgr=adjust_gamma(bgr,2)
+        bgr, hsv = brightnessBump(bgr, 50)
+        bgr = adjust_gamma(bgr, 0.1)
+    elif colorType=='Purple':
+        bgr = cv2.normalize(resized, None, alpha=50, beta=200, norm_type=cv2.NORM_MINMAX)
+        bgr = cv2.dilate(bgr, np.ones((3, 3), np.uint8))
+        bgr = cv2.medianBlur(bgr, 5)
+        bgr, hsv=brightnessBump(bgr,250)
+        bgr=adjust_gamma(bgr,2)
+        bgr, hsv = brightnessBump(bgr, 50)
+        bgr = adjust_gamma(bgr, 0.1)
+    else:
+        bgr = cv2.normalize(resized, None, alpha=50, beta=200, norm_type=cv2.NORM_MINMAX)
+        bgr = cv2.dilate(bgr, np.ones((3, 3), np.uint8))
+        bgr = cv2.medianBlur(bgr, 5)
+        bgr, hsv=brightnessBump(bgr,250)
+        bgr=adjust_gamma(bgr,2)
+        bgr, hsv = brightnessBump(bgr, 50)
+        bgr = adjust_gamma(bgr, 0.1)
+    return bgr,hsv
 
-def colorIsolation(hsv_image,kernel_erode,lower_Green,upper_Green,lowerRed,upperRed,lowerYellow,upperYellow,lowerPurple,upperPurple):
-    # GREEN
-    greenMask = cv2.inRange(hsv_image, lower_Green, upper_Green)
-    GreenMasked = cv2.bitwise_and(hsv_image, hsv_image, mask=greenMask)
-    Green = cv2.erode(GreenMasked, kernel_erode, iterations=1)
-    resultGreen = cv2.medianBlur(Green, 7)
-    hsv_image-=GreenMasked
-    # YELLOW
-    yellowMask = cv2.inRange(hsv_image, lowerYellow, upperYellow)
-    YellowMasked=cv2.bitwise_and(hsv_image,hsv_image,mask=yellowMask)
-    Yellow = cv2.erode(YellowMasked,kernel_erode,iterations=1)
-    resultYellow = cv2.medianBlur(Yellow, 7)
-    hsv_image-=YellowMasked
-    # PURPLE
-    purpleMask = cv2.inRange(hsv_image, lowerPurple, upperPurple)
-    PurpleMasked=cv2.bitwise_and(hsv_image,hsv_image,mask=purpleMask)
-    Purple=cv2.erode(PurpleMasked,kernel_erode,iterations=1)
-    resultPurple=cv2.medianBlur(Purple,7)
-    #RED
-    redMask = cv2.inRange(hsv_image, lowerRed, upperRed)
-    RedMasked = cv2.bitwise_and(hsv_image, hsv_image, mask=redMask)
-    Red = cv2.erode(RedMasked, kernel_erode, iterations=1)
-    resultRed = cv2.medianBlur(Red, 7)
-    #RETURN
-    return greenMask,resultGreen,yellowMask,resultYellow,redMask,resultRed,purpleMask,resultPurple
+def createMask(hsv,H_l, S_l, V_l,H_h, S_h, V_h,morphology=False):
+    mask = cv2.inRange(hsv, np.array([H_l, S_l, V_l]), np.array([H_h, S_h, V_h]))
+    if morphology:
+        kernel = np.ones((5, 5), np.uint8)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
+        mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    masked = cv2.bitwise_and(hsv, hsv, mask=mask)
+    return mask,masked
 
-
-def colorValidation(color, lowerTH, upperTH ):
-    if color[0][0][0] == color[0][0][1] == color[0][0][2] == 0:
-        if not lowerTH[0] < color[0][0][0] < upperTH[0]:
-            if not lowerTH[1] < color[0][0][1] < upperTH[1]:
-                if not lowerTH[2] < color[0][0][2] < upperTH[2]:
-                    return False
-    return True
-
-def aqColor(image, x, y):
-    B = image[x, y][0]
-    G = image[x, y][1]
-    R = image[x, y][2]
-    return cv2.cvtColor(np.uint8([[[B, G, R]]]), cv2.COLOR_BGR2HSV)
-
-def findCircles(mask, isoColor, low, high, dp, minDist, param_1, param_2, minRadius, maxRadius, rangeSize):
-    circles = cv2.HoughCircles(mask, cv2.HOUGH_GRADIENT, dp, minDist, param1=param_1, param2=param_2, minRadius=minRadius, maxRadius=maxRadius)
-    realCircles=[]
-    try:
-        circles = np.uint16(np.around(circles))
-        inRange = False
-        rangeSize = 3
-        minusCounter = 0
-        for i in circles[0, :]:
-            color = aqColor(isoColor, i[1], i[0])
-            inRange = colorValidation(color, low, high)
-            for z in range(-rangeSize, rangeSize):
-                for k in range(-rangeSize, rangeSize):
-                    inRange = colorValidation(color, low, high)
-                    color = aqColor(isoColor, i[1] + k, i[0] + z)
-                    if inRange:
-                        break
-                if inRange:
-                    break
-            if not inRange:
-                minusCounter += 1
-        ### visualisation debuging ###
-        #   if inRange:
-        #       cv2.circle(isoColor, (i[0], i[1]), i[2], (0, 0, 255), 1)
-        #       cv2.circle(isoColor, (i[0], i[1]), 2, (0, 0, 255), 1)
-        # cv2.imshow('circles', isoColor)
-        result = len(circles[0]) - minusCounter
-        return result
-    except:
-        result=0
-        return result
+def searchCountours(mask,image):
+    cont, hier = cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_TC89_L1)
+    count=0
+    for contour in cont:
+        #DEBUG BEGIN
+        '''
+        if debug_enabled:
+            cv2.drawContours(image,[contour],-1,(0,255,0),2)
+        '''
+        #DEBUG END
+        count+=1
+    return count
 
 def detect(img_path: str) -> Dict[str, int]:
     """Object detection function, according to the project description, to implement.
@@ -111,47 +118,62 @@ def detect(img_path: str) -> Dict[str, int]:
     Dict[str, int]
         Dictionary with quantity of each object.
     """
-    #INITIALISATION
-    ##VARIABLES
-    minDist = 30
-    minRadius = 2
-    maxRadius = 18
-    param_1 = 200
-    param_2 = 2.5
-    dp = 1
-    rangeSize = 3
-    ## MASKS
-    ### GREEN
-    lowerGreen = np.array([30, 150, 0])
-    upperGreen = np.array([70, 255, 255])
-    ### YELLOW
-    lowerYellow = np.array([0, 120, 135])
-    upperYellow = np.array([30, 255, 255])
-    ### RED
-    lowerRed = np.array([160, 40, 130])
-    upperRed = np.array([200, 255, 255])
-    ### PURPLE
-    lowerPurple = np.array([155, 60, 0])
-    upperPurple = np.array([180, 255, 255])
-    ## KERNELS
-    kernel_erode = np.ones((3, 3), np.uint8)
-    #END INITIALISATION
-    #CODE
-    ogImage= cv2.imread(img_path, cv2.IMREAD_COLOR)
-    resized, blured, hsv, brightenedHSV, brightenedBGR=imageInitialProcessing(ogImage,30)
-    greenMask,resultGreen,yellowMask,resultYellow,redMask,resultRed,purpleMask,resultPurple = colorIsolation(brightenedHSV,kernel_erode,lowerGreen,upperGreen,lowerRed,upperRed,lowerYellow,upperYellow,lowerPurple,upperPurple)
-    #END CODE
-    #ANSWER
-    redAns = findCircles(redMask, resultRed, lowerRed, upperRed,dp,minDist,param_1,param_2,minRadius,maxRadius,rangeSize)
-    yellowAns = findCircles(yellowMask, resultYellow, lowerYellow, upperYellow,dp,minDist,param_1,param_2,minRadius,maxRadius,rangeSize)
-    greenAns = findCircles(greenMask, resultGreen, lowerGreen, upperGreen,dp,minDist,param_1,param_2,minRadius,maxRadius,rangeSize)
-    purpleAns = findCircles(purpleMask, resultPurple, lowerPurple, upperPurple,dp,minDist,param_1,param_2,minRadius,maxRadius,rangeSize)
-    #END ANSWER
-    #DEBUG
-    cv2.waitKey()
+    #READ IMAGE
+    ogImage = cv2.imread(img_path, cv2.IMREAD_COLOR)
+    #DEBUG BEGIN
+    '''
+    while debug_enabled:
+        rgb, hsv = imageInitialProcessing(ogImage,'Green')
+        H_lower = cv2.getTrackbarPos('H-Lower', 'image')
+        S_lower = cv2.getTrackbarPos('S-Lower', 'image')
+        V_lower = cv2.getTrackbarPos('V-Lower', 'image')
+        H_higher = cv2.getTrackbarPos('H-Higher', 'image')
+        S_higher = cv2.getTrackbarPos('S-Higher', 'image')
+        V_higher = cv2.getTrackbarPos('V-Higher', 'image')
+        tempMaskLow=np.array([H_lower,S_lower,V_lower])
+        tempMaskHigh = np.array([H_higher, S_higher, V_higher])
+        tempMask = cv2.inRange(hsv, tempMaskLow, tempMaskHigh)
+        if True:
+            kernel = np.ones((5, 5), np.uint8)
+            tempMask = cv2.morphologyEx(tempMask, cv2.MORPH_CLOSE, kernel)
+            tempMask = cv2.morphologyEx(tempMask, cv2.MORPH_OPEN, kernel)
+        tempMasked = cv2.bitwise_and(hsv, hsv, mask=tempMask)
+        count = searchCountours(tempMask,rgb)
+        cv2.imshow('image', rgb)
+        cv2.imshow('mask', tempMask)
+        key_code = cv2.waitKey(10)
+        if key_code == 27: # escape key pressed
+            #print(count)
+            break
+    '''
     #DEBUG END
-    #RETURN
-    return {'red': redAns, 'yellow': yellowAns, 'green': greenAns, 'purple': purpleAns}
+    #CODE BEGINS
+    # # YELLOW BEGIN # #
+    rgbY, hsvY = imageInitialProcessing(ogImage,'Yellow')
+    yellowMask,yellowMasked=createMask(hsvY,12,40,20,30,255,255,morphology=True)
+    yellowCount = searchCountours(yellowMask, yellowMasked)
+    # # YELLOW END   # #
+    # # GREEN BEGIN # #
+    rgbG, hsvG = imageInitialProcessing(ogImage, 'Green')
+    greenMask, greenMasked = createMask(hsvG, 40, 0, 9, 80, 255, 255, morphology=True)
+    greenCount = searchCountours(greenMask, greenMasked)
+    # # GREEN END   # #
+    # # RED BEGIN # #
+    rgbR, hsvR = imageInitialProcessing(ogImage, 'Red')
+    redMask, redMasked = createMask(hsvR, 172, 15, 231, 179, 255, 255, morphology=True)
+    redCount = searchCountours(redMask, redMasked)
+    # # RED END   # #
+    # # PURPLE BEGIN # #
+    rgbP, hsvP = imageInitialProcessing(ogImage, 'Purple')
+    purpleMask, purpleMasked = createMask(hsvP, 130, 17, 0, 176, 255, 241, morphology=True)
+    purpleCount = searchCountours(purpleMask, purpleMasked)
+    # # PURPLE END   # #
+    #DEBUG BEGIN
+    '''
+    print('\nRED=',redCount,'\nYELLOW=',yellowCount,'\nGREEN=',greenCount,'\nPURPLE=',purpleCount)
+    '''
+    #DEBUG END
+    return {'red': redCount, 'yellow': yellowCount, 'green': greenCount, 'purple': purpleCount}
 
 @click.command()
 @click.option('-p', '--data_path', help='Path to data directory', type=click.Path(exists=True, file_okay=False, path_type=Path), required=True)
